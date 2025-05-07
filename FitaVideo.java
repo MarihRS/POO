85dbc53cb2dcb2c9bc5a433c5209d8e77608efc3
Marirdgs <87981811+MarihRS@users.noreply.github.com>
2025-05-07 20:29:51 -0300

    Add files via upload

---
+++ FitaVideo.java
@@ -0,0 +1,32 @@
+//CLASSE
+public class FitaVideo {
+    
+//ATRIBUTOS
+    private String titulo;
+    private double precoPorDia; 
+
+//CONSTRUTOR
+    public FitaVideo(String titulo, double precoPorDia){
+        this.titulo = titulo;
+        this.precoPorDia = precoPorDia;
+    }
+
+//MÉTODO
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public double getPrecoPorDia(){
+        return precoPorDia;
+    }
+
+    public double getValorAluguel(int numeroDeDiasAlugada){
+        return precoPorDia * numeroDeDiasAlugada;
+    }
+
+    public void imprimir(int numeroDeDiasAlugada){
+        System.out.printf("Título: %s" \n, titulo); //%n = adiciona uma nova linha
+        System.out.printf("Preço do aluguel por dia: R$ %.2f" \n, precoPorDia);
+        System.out.printf("Valor do aluguel para %d dias: R$ %.2" \n , numeroDeDiasAlugada, getValorAluguel(numeroDeDiasAlugada));
+    }
+}
---
+++ PrincipalFitaVideo.java
@@ -0,0 +1,8 @@
+public class PrincipalFitaVideo {
+    public static void main(String[] args){
+        FitaVideo f = new FitaVideo("It: A Coisa", 5.00);
+        f.imprimir();
+    }
+}
+
+//não consegui encontrar o erro :/
