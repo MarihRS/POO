8b3c4f3feb659c366a921a60df25bcac7d3ced23
Marirdgs <87981811+MarihRS@users.noreply.github.com>
2025-05-07 19:25:27 -0300

    Add files via upload

---
+++ Circulo.java
@@ -0,0 +1,24 @@
+//CLASSE
+public class Circulo {
+    
+//ATRIBUTOS
+    private int raio;
+
+//CONSTRUTOR
+    public Circulo(int raio){
+        this.raio = raio;
+    }
+
+//MÉTODO
+    public int calcularArea(){
+        return 3,141516 * raio * raio;
+    }
+
+    public int calcularPerimetro(){
+        return 2 * 3,141516 * raio
+    }
+
+    public void imprimir(){
+        System.ou.printf("Raio: %d, Área: %d, Perímetro: %d \n", raio, calcularArea(), calcularPerimetro());
+    }
+}
