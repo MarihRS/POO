ceeae58cd729537a5f749ac25a7ca212f5f29d51
Mari Rodrigues <87981811+MarihRS@users.noreply.github.com>
2025-05-14 10:50:57 -0300

    Add files via upload

--- FitaVideo.java
+++ FitaVideo.java
@@ -25,8 +25,8 @@ public class FitaVideo {
     }
 
     public void imprimir(int numeroDeDiasAlugada){
-        System.out.printf("Título: %s", titulo); //%n = adiciona uma nova linha
-        System.out.printf("Preço do aluguel por dia: R$ %.2f", precoPorDia);
-        System.out.printf("Valor do aluguel para %d dias: R$ %.2", numeroDeDiasAlugada, getValorAluguel(numeroDeDiasAlugada));
+        System.out.printf("Título: %s \n", titulo); //%n = adiciona uma nova linha
+        System.out.printf("Preço do aluguel por dia: R$ %.2f \n", precoPorDia); //%.2f = pra float ou double
+        System.out.printf("Valor do aluguel para %d dias: R$ %.2f \n", numeroDeDiasAlugada, getValorAluguel(numeroDeDiasAlugada)); //%d = inteiro
     }
 }
--- PrincipalFitaVideo.java
+++ PrincipalFitaVideo.java
@@ -1,8 +1,6 @@
-public class PrincipalFitaVideo {
-    public static void main(String[] args){
-        FitaVideo f = new FitaVideo("It: A Coisa", 5.00);
-        f.imprimir();
+public class PrincipalFitaVideo{
+    public static void main(String[] args){ //É PADRÃO, >>NÃO MUDA<<
+        FitaVideo f = new FitaVideo("Divergente", 5.00);
+        f.imprimir(4); //tem que especificar o número de dias alugada, nesse caso 4
     }
 }
-
-//não consegui encontrar o erro :/
