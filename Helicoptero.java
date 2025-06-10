00168398ea83b28f872f62115891ab51cb428f8e
Mari Rodrigues <87981811+MarihRS@users.noreply.github.com>
2025-06-10 17:16:03 -0300

    Atualizar o Helicoptero.java

--- Helicoptero.java
+++ Helicoptero.java
@@ -1,3 +1,4 @@
+
 //CLASSE
 public class Helicoptero {
 
@@ -7,113 +8,111 @@ public class Helicoptero {
     private int capacidade;
     private int qntd_pessoas;
 
-//CONSTRUTOR  só a capacidade
-    public Helicoptero (int capacidade){
+//CONSTRUTOR - apenas a capacidade
+    public Helicoptero(int capacidade) {
         this.capacidade = capacidade;
     }
 
-//MÉTODOS if, get e set, toString
+//MÉTODO 1 - if e else
 
-//Método 1: IF e ELSE
-    public void entra(int capacidade){
-        if(capacidade > qntd_pessoas) {
-            capacidade = capacidade + 1;
-            System.out.printf("Pode entrar");
-        }
-
-        else{
-            System.out.printf("Não pode.");
+    public void adicionaPessoa() {
+        if (qntd_pessoas < capacidade) {
+            qntd_pessoas++;
+            System.out.println("Pessoa adicionada.");
+        } else {
+            System.out.println("Capacidade máxima atingida. Não é possível adicionar mais pessoas.");
         }
     }
 
-    public void sai(int qntd_pessoas){
-        if(qntd_pessoas > 0){
-            qntd_pessoas = qntd_pessoas - 1;
-            System.out.printf("Saia.");
-        }    
-
-        else{
-            System.out.printf("O helicóptero já está vazio.");
+    public void removePessoa() {
+        if (qntd_pessoas > 0) {
+            qntd_pessoas--;
+            System.out.println("Pessoa removida.");
+        } else {
+            System.out.println("O helicóptero já está vazio.");
         }
     }
 
-    public void ligar(boolean ligado){
-        if(ligado == false){
-            System.out.printf("Ligar o helicóptero.");
-        }
-        
-    else{
-            System.out.printf("O helicóptero já está ligado.");
-        }
-    }
-
-    public void decolar(double altitude){
-        if(ligado == true && altitude == 0){
-            this.altitude = altitude;
-            System.out.printf("Decolar!!");
-        }
-
-        else{
-            System.out.printf("Não pode decolar.");
+    public void ligaHelicoptero() {
+        if (!ligado) {
+            ligado = true;
+            System.out.println("Helicóptero ligado.");
+        } else {
+            System.out.println("O helicóptero já está ligado.");
         }
     }
 
-    public void aterrissar(){
-        if(ligado == true && altitude > 0){
-            System.out.printf("Pode aterrissar.");
-        }
-
-        else{
-            System.out.printf("Não é possível aterrissar.");
-        }
-    }
-
-    public void desligar(){
-        if(ligado == true && altitude == 0){
-            System.out.printf("Pode desligar.");
-        }
-
-        else{
-            System.out.printf("Não pode desligar.");
+    public void helicopteroDecola(double altura) {
+        if (ligado && altitude == 0) {
+            altitude = altura;
+            System.out.println("Decolagem realizada.");
+        } else {
+            System.out.println("Não é possível decolar.");
         }
     }
 
-//Método 2 - get (retorna) e set (modifica)
+    public void helicopteroAterrissar(double altura) {
+        if (ligado && altitude >= altura) {
+            altitude -= altura;
+            System.out.println("Aterrissagem realizada.");
+        } else {
+            System.out.println("Não é possível aterrissar.");
+        }
+    }
 
-    public boolean getLigado(){
+    public void helicopteroDesliga() {
+        if (ligado && altitude == 0) {
+            ligado = false;
+            System.out.println("Helicóptero desligado.");
+        } else {
+            System.out.println("Não é possível desligar o helicóptero.");
+        }
+    }
+
+//MÉTODO 2 - get e set
+
+    public boolean getLigado() {
         return ligado;
     }
-    public void setLigado(boolean ligado){
+
+    public void setLigado(boolean ligado) {
         this.ligado = ligado;
     }
 
-    public double getAltitude(){
+    public double getAltitude() {
         return altitude;
     }
-    public void setAltitude(double altitude){
+
+    public void setAltitude(double altitude) {
         this.altitude = altitude;
     }
 
-    public int getCapacidade(){
+    public int getCapacidade() {
         return capacidade;
     }
-    public void setCapacidade(int capacidade){
+
+    public void setCapacidade(int capacidade) {
         this.capacidade = capacidade;
     }
 
-    public int getQntd_pessoas(){
+    public int getQntd_pessoas() {
         return qntd_pessoas;
     }
-    public void setQntd_pessoas(int qntd_pessoas){
+
+    public void setQntd_pessoas(int qntd_pessoas) {
         this.qntd_pessoas = qntd_pessoas;
     }
 
-    public String toString(){
-        return "Ligado:" + ligado + "Altitude: " + altitude + "Capacidade: " + capacidade + "Quantidade de pessoas: " + qntd_pessoas;
+    //MÉTODO - toString
+    public String toString() {
+        return "Ligado: " + ligado +
+               "\nAltitude: " + altitude +
+               "\nCapacidade: " + capacidade +
+               "\nQuantidade de pessoas: " + qntd_pessoas;
     }
 
-//Método 3: imprimir
-    public void imprimir(){
-        System.out.printf("Capacidade: %d", capacidade);
+//MÉTODO 3 - imprimir
+    public void imprimir() {
+        System.out.printf("Capacidade: %d\n", capacidade);
     }
 }
