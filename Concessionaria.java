ab16ab3871f88c6945fe03780622e3dfc0eccbac
Mari Rodrigues <87981811+MarihRS@users.noreply.github.com>
2025-11-25 17:16:53 -0300

    Add files via upload

---
+++ Concessionaria.java
@@ -0,0 +1,73 @@
+public class Concessionaria {
+    private Veiculo[] veiculos;
+    private int qntdVeiculos;
+
+    public Concessionaria(){
+        this.veiculos = new Veiculo[10];
+        this.qntdVeiculos = 0;
+    }
+
+//MÉTODOS
+//1 - para adicionar veículo a concessionária;
+    public void adicionarVeiculo(Veiculo v){
+        if(qntdVeiculos == this.veiculos.length)
+        veiculos[qntdVeiculos++] = v;
+    }
+
+//2- para listar veículos da concessionária;
+    public void listarVeiculos(){ 
+        System.out.println("\n == Veículos Cadastrados ==");
+        for(int i = 0; i < qntdVeiculos; i++){
+            System.out.println(veiculos[i].toString());
+        }
+    }
+
+//3- para calcular o valor total do preço de venda dos veículos da concessionária;
+    public double calcularValorTotalEstoque() {
+        double totalEstoque = 0;
+        for (int i = 0; i < qntdVeiculos; i++){
+            totalEstoque += veiculos[i].calcularPrecoVenda();
+        }
+        return totalEstoque;
+    }
+
+//4- para buscar um veículo pela marca na concessionária;
+    public void buscarPorMarca(String marca) {
+        System.out.println("\n== Resultado da Busca ==");
+        boolean encontrou = false;
+
+        for (int i = 0; i < qntdVeiculos; i++){ 
+            if (veiculos[i].getMarca().equals(marca)) { //equals- 
+                System.out.println(veiculos[i].toString());
+                encontrou = true;
+            }
+        }
+        if (!encontrou) {
+            System.out.println("Nenhum veículo encontrado.");
+        }
+    }
+
+//5- para retornar o veículo mais caro da concessionária;
+    public Veiculo veiculoMaisCaro(){
+        Veiculo maisCaro = veiculos[0]; //começa com o primeiro veículo
+        for (int i = 1; i < qntdVeiculos; i++) { //i recebe 1 pq o índice 0 já tá sendo usado como valor inicial
+            if(veiculos[i].calcularPrecoVenda() > maisCaro.calcularPrecoVenda()) {
+                maisCaro = veiculos[i]; //atualiza para esse ser o mais caro
+            }
+        }
+        return maisCaro;
+    }
+
+//6- para calcular o valor total do seguro dos veículos da concessionária.
+    public double calcularSeguroTotal() {
+        double totalSeguro = 0;
+        for(int i = 0; i < qntdVeiculos; i++) {
+            totalSeguro += veiculos[i].calcularSeguro();
+        }
+        return totalSeguro;
+    }
+
+    public int getQntdVeiculos(){
+        return qntdVeiculos;
+    }
+}
---
+++ PrincipalConcessionaria.java
@@ -0,0 +1,45 @@
+public class PrincipalConcessionaria {
+    public static void main(String[] args) {
+        
+//Crie um objeto
+        Concessionaria c = new Concessionaria();
+    
+//Adicione alguns Carros, Motos e Caminhões
+    Carro carro1 = new Carro("Volkswagen", "Gol", 2010, 23000.0, 4, "Flex", 76.0);
+    Carro carro2 = new Carro("Honda", "Civil EXL", 2021, 155000.0, 4, "Flex", 173.0);
+    c.adicionarVeiculo(carro1);
+    c.adicionarVeiculo(carro2);
+
+    Moto moto1 = new Moto("Honda", "CG 160", 2023, 15000.0, 160, "Elétrica", true);
+    Moto moto2 = new Moto("Yamaha", "MT-09", 2024, 50000.0, 847, "Elétrica", false);
+    c.adicionarVeiculo(moto1);
+    c.adicionarVeiculo(moto2);
+
+    Caminhao caminhao1 = new Caminhao("Volvo", "FH 540", 2023, 350000.0, 25.0, 6, "Baú");
+    Caminhao caminhao2 = new Caminhao("Mercedes", "Actros", 2024, 400000.0, 30.0, 7, "Sider");
+    c.adicionarVeiculo(caminhao1);
+    c.adicionarVeiculo(caminhao2);
+
+    System.out.println("\nTotal de veículos cadastrados: " + c.getQntdVeiculos());
+
+//Liste os veículos cadastrados.
+    c.listarVeiculos();
+
+//Mostre o valor total do estoque.
+    double totalEstoque = c.calcularValorTotalEstoque();
+
+//Busque e exiba um veículo pela marca.
+    c.buscarPorMarca("Mercedes");
+    c.buscarPorMarca("Ford");
+    c.buscarPorMarca("Yamaha");
+
+//Exibir o veículo mais caro.
+        Veiculo maisCaro = c.veiculoMaisCaro();
+        if (maisCaro != null) {
+            System.out.println(maisCaro.toString());
+        }
+
+//Exibir o total anual de seguro.
+    double totalSeguro = c.calcularSeguroTotal();
+    }
+}
